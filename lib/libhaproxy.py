3f83de8d687a9da79b6df965ad5039f58e1866ba
Chris Sanders <sanders.chris@gmail.com>
2017-06-27 21:53:44 -0500

    Force an open call even if ports already open for renew action

--- lib/libhaproxy.py
+++ lib/libhaproxy.py
@@ -316,4 +316,11 @@ class ProxyHelper():
 
     def renew_upnp(self):
         hookenv.log("Renewing upnp port requests","INFO")
+        # check that open ports is accurate
         self.update_ports()
+        # send upnp for ports even if they were already open
+        opened_ports = str(subprocess.check_output(["opened-ports"]),'utf-8').split('/tcp\n')
+        for port in opened_ports:
+            hookenv.log("Opening port {}".format(frontend.port),"INFO")
+            hookenv.open_port(frontend.port)
+
