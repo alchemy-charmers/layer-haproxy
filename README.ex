767dcf662cdef04c91fb14c330804a80ef648eec
Chris Sanders <sanders.chris@gmail.com>
2017-07-18 15:38:59 -0500

    Initial README

--- README.ex
+++ README.ex
@@ -1,65 +1,72 @@
 # Overview
 
-Describe the intended usage of this charm and anything unique about how this
-charm relates to others here.
-
-This README will be displayed in the Charm Store, it should be either Markdown
-or RST. Ideal READMEs include instructions on how to use the charm, expected
-usage, and charm features that your audience might be interested in. For an
-example of a well written README check out Hadoop:
-http://jujucharms.com/charms/precise/hadoop
-
-Use this as a Markdown reference if you need help with the formatting of this
-README: http://askubuntu.com/editing-help
-
-This charm provides [service][]. Add a description here of what the service
-itself actually does.
-
-Also remember to check the [icon guidelines][] so that your charm looks good
-in the Juju GUI.
+This charm provides [HAProxy][haproxy]. HAProxy describes itself as a free, very
+fast and reliable solution offering high availability, load balancing, and
+proxying for TCP and HTTP-based applications. It is particularly suited for very
+high traffic web sites and powers quite a number of the world's most visited
+ones. Over the years it has become the de-facto standard opensource load
+balancer, is now shipped with most mainstream Linux distributions, and is often
+deployed by default in cloud platforms. Since it does not advertise itself, we
+only know it's used when the admins report it :-) 
 
 # Usage
 
-Step by step instructions on using the charm:
+To deploy:
 
-juju deploy servicename
+    juju deploy haproxy
 
-and so on. If you're providing a web service or something that the end user
-needs to go to, tell them here, especially if you're deploying a service that
-might listen to a non-default port.
-
-You can then browse to http://ip-address to configure the service.
-
-## Scale out Usage
-
-If the charm has any recommendations for running at scale, outline them in
-examples here. For example if you have a memcached relation that improves
-performance, mention it here.
+You will most likely want to use a bundle to set options during deployment. The
+primary use case for this charm is to allow other chamrs that implemnt the
+[reverse proxy][interface-reverseproxy] interface to automatically register for
+reverse proxy. This charm will allow both http and tcp reverse proxy's to be
+requested from other charms. Addationally, for http reverse proxy Letsencrypt is
+provided to allow HAProxy to automatically register for a certificate and
+terminate the SSL/TLS connection. Finally, this charm provides options to use
+UPNP for automaticly requesting ports via UPNP. Most of these featuers are off
+by default, see the configuration options to enable them.
 
 ## Known Limitations and Issues
 
-This not only helps users but gives people a place to start if they want to help
-you add features to your charm.
+This charm is under development, several other usecases/features are still under
+consideration. Merge requests are certinaly apprecaited, some examples of
+current limitations include.
+
+ * No way to configure reverseproxy for non-charmed services
+ * No HA Failover or Scaleout usage currently implented
+ * Can not restrict the ports other charms request
+ * Unit/Functional testing is not yet implemented
 
 # Configuration
 
-The configuration options will be listed on the charm store, however If you're
-making assumptions or opinionated decisions in the charm (like setting a default
-administrator password), you should detail that here so the user knows how to
-change it immediately, etc.
+See the full list of configuration options below. This will detail some of the
+options that are worth highlighting.
+
+ - Version: This is tested at the deafult '1.7', it is intended to provide a way
+   to specify a different repository version. In theory if new versions are
+   backwards compatible chaning this is all that will be needed for new
+   releases.
+ - To access HAProxy stats please see "stats-user", "stats-passwd", "stats-url",
+   "stats-port", and "stats-local" configuration settings. Note that the stats
+   port must be unique, if you want to use the default port of 9000 for other
+   service you should change this setting.
+ - UPNP is provided via monkey patch and should be considered a convinence.
+   Running UPNP in production is not recommended practice.
+ - hostname will allow you to customize the hostname of HAProxy, be aware that
+   doing this can cause multiple hosts to have the same hostname if you scale
+   out the number of units.
 
 # Contact Information
 
 Though this will be listed in the charm store itself don't assume a user will
 know that, so include that information here:
 
-## Upstream Project Name
+## Upstream Project Information
 
-  - Upstream website
-  - Upstream bug tracker
-  - Upstream mailing list or contact information
-  - Feel free to add things if it's useful for users
+  - Code: https://github.com/chris-sanders/layer-haproxy 
+  - Bug tracking: https://github.com/chris-sanders/layer-haproxy/issues
+  - Contact information: sanders.chris@gmail.com
 
+[haproxy]: http://www.haproxy.org/
+[interface-reverseproxy]:
+https://github.com/chris-sanders/interface-reverseproxy
 
-[service]: http://example.com
-[icon guidelines]: https://jujucharms.com/docs/stable/authors-charm-icon
