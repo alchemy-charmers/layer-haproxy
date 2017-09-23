11c970d750ff8f0331577b1814de075061eb51a9
Chris Sanders <sanders.chris@gmail.com>
2017-09-23 17:32:53 -0500

    Need to mock other layers for example charm.layer.options

--- tests/test_hello.py
+++ tests/test_hello.py
@@ -3,6 +3,16 @@
 # import requests
 # import unittest
 
+# try:
+#     from libhaproxy import ProxyHelper
+# except:
+#     subprocess.check_call('2to3-3.5 -w /usr/local/lib/python3.5/dist-packages/pyhaproxy', shell=True)
+#     from libhaproxy import ProxyHelper
+
+from libhaproxy import ProxyHelper
+
+# ph = ProxyHelper()
+
 
 class TestHello():
     def hello(self):
@@ -10,3 +20,7 @@ class TestHello():
 
     def test_hello(self):
         assert self.hello() == "Hello"
+
+    def test_ph(self):
+        ph = ProxyHelper()
+        assert isinstance(ph.charm_config, dict)
